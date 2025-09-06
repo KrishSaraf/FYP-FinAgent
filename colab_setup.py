b89ac79cb69e03beff8e1f00db9c9b97ed694564
Pranav Garg <pranav020@e.ntu.edu.sg>
2025-09-06 11:14:24 +0800

    Comment out A100 setup and package installation commands
    Comment out setup commands for A100 optimization and package installations.

--- colab_setup.py
+++ colab_setup.py
@@ -1,18 +1,18 @@
 # Google Colab A100 Optimization Setup
 
-# Enable A100 high performance mode
-!nvidia-smi -pm 1
-!nvidia-smi -ac 1215,1410
+# # Enable A100 high performance mode
+# !nvidia-smi -pm 1
+# !nvidia-smi -ac 1215,1410
 
-# Install JAX with CUDA support
-!pip install --upgrade pip
-!pip install jax[cuda11_pip]==0.4.13 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-!pip install flax optax chex gymnax
-!pip install wandb
+# # Install JAX with CUDA support
+# !pip install --upgrade pip
+# !pip install jax[cuda11_pip]==0.4.13 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+# !pip install flax optax chex gymnax
+# !pip install wandb
 
-# Install additional GPU-accelerated packages
-!pip install cupy-cuda11x  # For NumPy replacement
-!pip install numba        # For custom kernels if needed
+# # Install additional GPU-accelerated packages
+# !pip install cupy-cuda11x  # For NumPy replacement
+# !pip install numba        # For custom kernels if needed
 
 # Set JAX memory preallocation (important for A100)
 import os
@@ -21,7 +21,7 @@ os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'true'
 
 # Enable JIT compilation caching
 os.environ['JAX_COMPILATION_CACHE_DIR'] = '/content/jax_cache'
-!mkdir -p /content/jax_cache
+# !mkdir -p /content/jax_cache
 
 # Verify GPU setup
 import jax
