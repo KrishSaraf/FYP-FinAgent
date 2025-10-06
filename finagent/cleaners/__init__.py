59c3906de205785c6c3bdb3e1ec007c6612e1ca3
KrishSaraf <krish.saraf@gmail.com>
2025-10-06 12:46:28 +0800

    Add only aligned CSV files for training data

--- finagent/cleaners/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Data cleaning module for Indian market data.
-This module provides functionality to clean and preprocess various types of market data files.
-"""
-
-from .price_cleaner import PriceCleaner
-
-__all__ = ['PriceCleaner']
